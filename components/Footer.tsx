2a6fa8b98f0c81abf77f8de8127bc7f1cbe49e34
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-14 10:45:43 +0100

    update(Footer): add styles to copyrights

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
 export const Footer = () => {
   return (
     <footer className="bg-indigo-600 text-white h-40 flex justify-center items-end">
-      <p className="">
+      <p className="p-4">
         ©
-        <a className="underline mx-2" href="mailto:jaseveen@gmail.com">
+        <a
+          className="underline decoration-wavy underline-offset-4 mx-2"
+          href="mailto:jaseveen@gmail.com"
+        >
           vseven
         </a>
         and the state of California
