74e9b9a94c7a69b707539122cf647410863b5cf8
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-14 10:41:28 +0100

    fix(DottedPagination): remove hidden on mobile

--- components/Pagination/DottedPagination.tsx
+++ components/Pagination/DottedPagination.tsx
@@ -14,7 +14,7 @@ export const DottedPagination = ({
 }: Props) => {
   return (
     <nav className="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
-      <div className="hidden md:-mt-px md:flex items-baseline">
+      <div className="flex items-baseline">
         <Link href={`/products${currentPage > 1 ? `/${currentPage - 1}` : ""}`}>
           <a className="mr-4 bg-indigo-600 hover:bg-indigo-700 text-white shadow-md p-2 disabled:bg-indigo-300">
             Prev
