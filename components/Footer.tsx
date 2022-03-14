459bd9ec1fbe900ce8674c650d59532b01ebb4bc
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-14 09:28:53 +0100

    feat: create Footer, Header and Main components

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-indigo-600 text-white h-40 flex justify-center items-end">
+      <p className="">
+        ©
+        <a className="underline mx-2" href="mailto:jaseveen@gmail.com">
+          vseven
+        </a>
+        and the state of California
+      </p>
+    </footer>
+  );
+};
