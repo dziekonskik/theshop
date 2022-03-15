55ed8e5998a81ef67a63ecf410cc6867a5e22a96
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-15 07:53:31 +0100

    feat(SEO): add next-seo.config and DefaultSeo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "../styles/globals.css";
 import "../styles/styles.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { DefaultSeo } from "next-seo";
 import { Layout } from "../components/layout/Layout";
+import SEO from "../next-seo.config";
 
 const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <DefaultSeo {...SEO} />
       <QueryClientProvider client={client}>
         <Component {...pageProps} />
       </QueryClientProvider>
