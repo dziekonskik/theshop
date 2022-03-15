d3dd4beb77db4be11fc45c04c00a3cf2b87fb07e
Konrad Dziekonski <jaseveen@gmail.com>
2022-03-15 07:55:13 +0100

    feat(react-markdown): add ZaisteReactMarkdown component

---
+++ components/ZaisteReactMarkdown.tsx
@@ -0,0 +1,23 @@
+import ReactMarkdown from "react-markdown";
+import Link from "next/link";
+
+export const ZaisteReactMarkdown = ({ children }: { children: string }) => {
+  return (
+    <ReactMarkdown
+      components={{
+        a: ({ href, ...props }) => {
+          if (!href) {
+            return <a {...props}></a>;
+          }
+          return (
+            <Link href={href}>
+              <a {...props}></a>
+            </Link>
+          );
+        },
+      }}
+    >
+      {children}
+    </ReactMarkdown>
+  );
+};
