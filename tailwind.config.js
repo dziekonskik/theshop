2ca003a36e170fc6aec07a6ebd6abaf339ea22a5
Konrad Dziekonski <jaseveen@gmail.com>
2022-04-20 21:57:13 +0200

    feat(assets): add fonts and payment svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        play: ["Play", "sans-serif"],
+        hubballi: ["Hubballi", "sans-serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
