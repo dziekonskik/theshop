9375063c31e4507cc6ec7e66e7b6a118a39a7c3c
Konrad Dziekonski <jaseveen@gmail.com>
2022-05-03 18:00:27 +0200

    refactor(styles): add styles for product in grid and details

--- components/Product/ProductDetails.tsx
+++ components/Product/ProductDetails.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { NextSeo } from "next-seo";
 import { Rating } from "./ProductRating";
+import { AddToCartButton } from "../AddToCartButton";
+import { useCartState } from "../Cart/CartContext";
 import { ProductReviewContainer } from "../ProductReview/ProductReviewContainer";
 import { ZaisteReactMarkdown } from "../ZaisteReactMarkdown";
-import type { MarkdownResult } from "../../util/types";
 import { ProductDetailsFragment } from "../../generated/graphql";
+import type { MarkdownResult } from "../../util/types";
 
 export interface ProductDetails extends ProductDetailsFragment {
   longDescription: MarkdownResult;
@@ -16,6 +18,7 @@ interface ProductProps {
 }
 
 export const ProductDetails = ({ data }: ProductProps) => {
+  const { addItemToCart } = useCartState();
   return (
     <>
       <NextSeo
@@ -37,20 +40,37 @@ export const ProductDetails = ({ data }: ProductProps) => {
         }}
       />
       <div className="bg-white p-4">
-        <Image
-          src={data.images[0].url}
-          alt={data.name}
-          layout="responsive"
-          objectFit="contain"
-          width={16}
-          height={9}
-        />
+        <div className="flex gap-7">
+          <div className="h-96 w-96">
+            <Image
+              src={data.images[0].url}
+              alt={data.name}
+              layout="responsive"
+              objectFit="fill"
+              width="100%"
+              height="100%"
+            />
+          </div>
+          <div className="p-4">
+            <h2 className="text-3xl font-bold">{data.name}</h2>
+            <article className="prose lg:prose-xl my-6">
+              <ZaisteReactMarkdown>{data.longDescription}</ZaisteReactMarkdown>
+            </article>
+            <AddToCartButton
+              onClick={() =>
+                addItemToCart({
+                  id: data.id,
+                  price: data.price,
+                  name: data.name,
+                  count: 1,
+                  slug: data.slug,
+                  images: data.images,
+                })
+              }
+            />
+          </div>
+        </div>
       </div>
-      <h2 className="p-4 text-3xl font-bold">{data.name}</h2>
-      <p className="p-4">{data.description}</p>
-      <article className="p-4 prose lg:prose-xl">
-        <ZaisteReactMarkdown>{data.longDescription}</ZaisteReactMarkdown>
-      </article>
       <Rating rating={data.rating} />
       <ProductReviewContainer slug={data.slug} />
     </>
--- components/Product/ProductListItem.tsx
+++ components/Product/ProductListItem.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { useCartState } from "../Cart/CartContext";
+import { AddToCartButton } from "../AddToCartButton";
 import type { ProductDetailsFragment } from "../../generated/graphql";
 
 interface ProductListItemProps {
@@ -12,37 +13,42 @@ export const ProductLstItem = ({ data }: ProductListItemProps) => {
 
   return (
     <>
-      <div className="bg-white p-4">
-        <Image
-          src={data.images[0].url}
-          alt={data.name}
-          layout="responsive"
-          objectFit="contain"
-          width={16}
-          height={9}
-        />
-      </div>
-      <div className="flex items-center">
+      <Link href={`/products/item/${data.slug}`}>
+        <a>
+          <div className="bg-white p-10 rounded-md">
+            <Image
+              src={data.images[0].url}
+              alt={data.name}
+              layout="responsive"
+              objectFit="contain"
+              width={16}
+              height={9}
+              className="hover:scale-125 transition-transform"
+            />
+          </div>
+        </a>
+      </Link>
+      <div className="flex flex-col items-center px-4">
         <Link href={`/products/item/${data.slug}`}>
           <a>
-            <h2 className="p-4 text-3xl font-bold">{data.name}</h2>
+            <h2 className="p-4 text-xl font-bold">{data.name}</h2>
           </a>
         </Link>
-        <button
-          onClick={() =>
-            addItemToCart({
-              id: data.id,
-              price: data.price,
-              name: data.name,
-              count: 1,
-              slug: data.slug,
-              images: data.images,
-            })
-          }
-          className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-        >
-          Kup to
-        </button>
+        <div className="flex items-center mb-4">
+          <div className="mr-3 text-xl">{data.price / 100} $</div>
+          <AddToCartButton
+            onClick={() =>
+              addItemToCart({
+                id: data.id,
+                price: data.price,
+                name: data.name,
+                count: 1,
+                slug: data.slug,
+                images: data.images,
+              })
+            }
+          />
+        </div>
       </div>
     </>
   );
