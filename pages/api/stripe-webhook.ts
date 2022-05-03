04160d33c7bf42f47f03efe06a10217929cac251
Konrad Dziekonski <jaseveen@gmail.com>
2022-05-03 14:21:24 +0200

    feat(stripe events): add typings for stripe events, add webhook api route

---
+++ pages/api/stripe-webhook.ts
@@ -0,0 +1,16 @@
+import { NextApiHandler } from "next";
+import type { StripeWebhookEvents } from "../../stripeEvents";
+
+const stripeWebhookHandler: NextApiHandler = (req, res) => {
+  console.log(req.body);
+  const event = req.body as StripeWebhookEvents;
+
+  switch (event.type) {
+    case "checkout.session.completed":
+      // TODO: zaktualizuj zamówienie w graphcms
+      return;
+  }
+  res.status(204).end();
+};
+
+export default stripeWebhookHandler;
