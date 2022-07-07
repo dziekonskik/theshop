b988712f1848ebfc70da2d6afa1e307690d6740c
Konrad Dziekonski <jaseveen@gmail.com>
2022-07-07 23:22:12 +0200

    refactor(bottom widget mobile): add summary widget on mobile

--- components/Cart/CartContent.tsx
+++ components/Cart/CartContent.tsx
@@ -1,8 +1,11 @@
+import { BigLink } from "../BigLink";
 import { CartContentListItem } from "./CartContentListItem";
 import { useCartState } from "./CartContext";
+import type { CartItem } from "../../util/types";
 
 export const CartContent = () => {
-  const { cartState, handleOrder } = useCartState();
+  const { cartState, handleOrder, calculateCartTotal } = useCartState();
+  const cartTotal = calculateCartTotal(cartState);
   return (
     <div className="col-span-12 lg:col-span-6 w-full">
       <div className="h-full lg:max-h-[550px] overflow-y-auto scrollbar w-full py-4">
@@ -20,7 +23,41 @@ export const CartContent = () => {
           </ul>
         </div>
       </div>
-      <div className="h-20 bg-gradient-to-b from-transparent to-gray-50 -translate-y-20"></div>
+      <SummaryBottomWidget cartState={cartState} cartTotal={cartTotal} />
     </div>
   );
 };
+
+interface SummaryBottomWidgetProps {
+  cartState: CartItem[];
+  cartTotal: number;
+}
+
+const SummaryBottomWidget = ({
+  cartState,
+  cartTotal,
+}: SummaryBottomWidgetProps) => (
+  <section className="bg-sunny bg-opacity-90 lg:hidden sticky bottom-0 z-10 flex p-4">
+    <div className="container mx-auto flex justify-between">
+      <summary className="flex flex-col w-1/2 md:w-1/3">
+        <div className="flex justify-between mt-4 font-acme text-xl">
+          <span className="capitalize">Items:</span>
+          <span>{cartState.length}</span>
+        </div>
+        <div className="flex justify-between mt-4 font-acme text-xl">
+          <span className="capitalize">Discount:</span>
+          <span>$ 0</span>
+        </div>
+        <div className="flex justify-between mt-4 font-acme text-xl">
+          <span className="uppercase mr-3">Total:</span>
+          <span>$ {cartTotal / 100}</span>
+        </div>
+      </summary>
+      <article className="flex justify-center items-center w-1/2 md:w-1/3">
+        <BigLink bgColor="#6C63FF" href="/checkout">
+          checkout
+        </BigLink>
+      </article>
+    </div>
+  </section>
+);
--- components/Cart/CartSummary.tsx
+++ components/Cart/CartSummary.tsx
@@ -8,7 +8,7 @@ export const CartSummary = () => {
     <section className="hidden col-span-6 lg:flex justify-end relative">
       <div className="mr-16 h-1/2 w-full bg-bermuda -skew-y-12 -z-10 border-4 border-midnight translate-y-5 flex justify-end relative">
         <div className="h-full skew-y-12 w-44 bg-sunny bg-opacity-90 absolute -right-16 top-10 scale-y-110"></div>
-        <article className="skew-y-12 mt-12 flex flex-col absolute right-11 w-44">
+        <summary className="skew-y-12 mt-12 flex flex-col absolute right-11 w-44">
           <h2 className="font-acme z-50 text-2xl capitalize">Summary:</h2>
           <div className="flex justify-between mt-8 font-acme z-50">
             <span className="capitalize">Items:</span>
@@ -22,7 +22,7 @@ export const CartSummary = () => {
             <span className="uppercase">Total:</span>
             <span>$ {cartTotal / 100}</span>
           </div>
-        </article>
+        </summary>
       </div>
       <div className="absolute bottom-[40%] right-32 ">
         <BigLink bgColor="#6C63FF" href="/checkout">
