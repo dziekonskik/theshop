5887684df8df3ef250656c63fafb6ffe609124db
Konrad Dziekonski <jaseveen@gmail.com>
2022-07-19 12:47:30 +0200

    refactor(webhook): add webhook validation

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -1,4 +1,5 @@
 import { NextApiHandler } from "next";
+import { Stripe } from "stripe";
 import { apolloClient } from "../../graphql/apolloClient";
 import {
   UpdateOrderDocument,
@@ -8,7 +9,27 @@ import {
 import type { StripeWebhookEvents } from "../../stripeEvents";
 
 const stripeWebhookHandler: NextApiHandler = (req, res) => {
-  const event = req.body as StripeWebhookEvents;
+  const webhookSignature = req.headers["stripe-signature"];
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  const stripeSecret = process.env.STRIPE_SECRET_KEY;
+
+  if (!stripeSecret || !webhookSignature || !webhookSecret) {
+    return res.status(500).json({ error: "Stripe credential not provided" });
+  }
+  const stripe = new Stripe(stripeSecret, { apiVersion: "2020-08-27" });
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      webhookSignature,
+      webhookSecret
+    ) as StripeWebhookEvents;
+  } catch (err: unknown) {
+    return res
+      .status(400)
+      .send(`Webhook Error: ${err instanceof Error && err.message}`);
+  }
 
   switch (event.type) {
     case "checkout.session.completed":
