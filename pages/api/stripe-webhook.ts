e8b9dc78b05183ef0362ae8a2ef6097530147531
Konrad Dziekonski <jaseveen@gmail.com>
2022-07-19 13:58:49 +0200

    refactor(webhook): install micro lib and disable body parser to provide raw req object to stripe

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -1,4 +1,5 @@
 import { NextApiHandler } from "next";
+import { buffer } from "micro";
 import { Stripe } from "stripe";
 import { apolloClient } from "../../graphql/apolloClient";
 import {
@@ -8,10 +9,20 @@ import {
 } from "../../generated/graphql";
 import type { StripeWebhookEvents } from "../../stripeEvents";
 
-const stripeWebhookHandler: NextApiHandler = (req, res) => {
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const stripeWebhookHandler: NextApiHandler = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).setHeader("Allow", "POST").end("Method Not Allowed");
+  }
   const webhookSignature = req.headers["stripe-signature"];
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   const stripeSecret = process.env.STRIPE_SECRET_KEY;
+  const reqAsBuffer = await buffer(req);
 
   if (!stripeSecret || !webhookSignature || !webhookSecret) {
     return res.status(500).json({ error: "Stripe credential not provided" });
@@ -21,7 +32,7 @@ const stripeWebhookHandler: NextApiHandler = (req, res) => {
   let event;
   try {
     event = stripe.webhooks.constructEvent(
-      req.body,
+      reqAsBuffer,
       webhookSignature,
       webhookSecret
     ) as StripeWebhookEvents;
