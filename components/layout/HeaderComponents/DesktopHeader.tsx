a54112f4811bffac690cbc21647772f54ddbf845
Konrad Dziekonski <jaseveen@gmail.com>
2022-07-25 11:14:44 +0200

    feat(DesktopHeader): add DesktopHeader component with NavLink component and animation for it

---
+++ components/layout/HeaderComponents/DesktopHeader.tsx
@@ -0,0 +1,27 @@
+import { Cart } from "../../Cart/Cart";
+import { NavLink } from "./NavLink";
+import type { NavItem } from "../Header";
+
+interface DesktopHeaderProps {
+  navItems: NavItem[];
+}
+
+export const DesktopHeader = ({ navItems }: DesktopHeaderProps) => {
+  return (
+    <section className="hidden md:flex h-full container mx-auto items-center justify-between">
+      <nav className="h-full">
+        <ul className="flex items-center h-full">
+          {navItems.map(({ url, text }, index) => (
+            <li
+              className={`text-midnight ${index > 0 ? "ml-20" : ""}`}
+              key={text}
+            >
+              <NavLink href={url}>{text}</NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Cart />
+    </section>
+  );
+};
---
+++ components/layout/HeaderComponents/NavLink.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import NextLink, { LinkProps } from "next/link";
+
+interface NavLinkProps extends LinkProps {
+  children: React.ReactNode;
+}
+
+export const NavLink = ({ children, href }: NavLinkProps) => {
+  const [hovered, setHovered] = useState(false);
+  return (
+    <NextLink href={href}>
+      <a
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className="font-comfortaa font-bold text-xl relative block"
+      >
+        {children}
+        <span
+          className={`absolute top-0 left-0 text-purple transition-all duration-700 ${
+            hovered ? "clip-path-revealed" : "clip-path-hidden"
+          }`}
+        >
+          {children}
+        </span>
+      </a>
+    </NextLink>
+  );
+};
