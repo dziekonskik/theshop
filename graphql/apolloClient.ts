6e817691f7e1faa692a783f9f7fc86593fdee0b1
Konrad Dziekonski <jaseveen@gmail.com>
2022-08-03 23:36:42 +0200

    fix(apolloClient): add NEXT_PUBLIC prefix to env variable

--- graphql/apolloClient.ts
+++ graphql/apolloClient.ts
@@ -1,12 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: process.env.HYGRAPH_CONTENT_API,
+  uri: process.env.NEXT_PUBLIC_HYGRAPH_CONTENT_API,
   cache: new InMemoryCache(),
 });
 
 export const personAuthApolloClient = new ApolloClient({
-  uri: process.env.HYGRAPH_CONTENT_API,
+  uri: process.env.NEXT_PUBLIC_HYGRAPH_CONTENT_API,
   cache: new InMemoryCache(),
   headers: {
     Authorization: `Bearer ${process.env.HYGRAPH_PERSON_TOKEN}`,
