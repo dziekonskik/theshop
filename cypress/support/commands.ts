11d81ceb231f1f49ca1d44900e01c2aa25369869
Konrad Dziekonski <jaseveen@gmail.com>
2022-09-07 11:02:04 +0200

    chore(cy test utils): add getByData command and graphQl test utils

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -25,13 +25,27 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+Cypress.Commands.add("getByData", (selector) => {
+  cy.get(`[data-testid=${selector}]`);
+});
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      // login(email: string, password: string): Chainable<void>;
+      // drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>;
+      // dismiss(
+      //   subject: string,
+      //   options?: Partial<TypeOptions>
+      // ): Chainable<Element>;
+      // visit(
+      //   originalFn: CommandOriginalFn,
+      //   url: string,
+      //   options: Partial<VisitOptions>
+      // ): Chainable<Element>;
+      getByData(selector: string): Chainable<Element>;
+    }
+  }
+}
+
+export {};
